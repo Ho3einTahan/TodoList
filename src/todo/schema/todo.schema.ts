da67c75bf339e66ea4de04eac427f050140337de
ho3eintahan <hoseintahan84@gmail.com>
2025-05-16 12:02:55 +0330

    first commit

---
+++ src/todo/schema/todo.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, HydratedDocument } from 'mongoose';
+
+export type TodoDocument = HydratedDocument<Todo>;
+
+@Schema()
+export class Todo {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop()
+  description?: string;
+
+  @Prop({ default: false })
+  done: boolean;
+}
+export const TodoSchema = SchemaFactory.createForClass(Todo);
