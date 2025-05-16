d97be4ef00fa65cfd48b7c792c740715139301b0
ho3eintahan <hoseintahan84@gmail.com>
2025-05-16 22:37:42 +0330

    Update   main.ts  to handle   `ejs`

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+const expressLayouts = require('express-ejs-layouts');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(expressLayouts);
+
+  app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
+  app.setViewEngine('ejs');
+  app.set('layout', 'layouts/layout');
+  app.use(expressLayouts);
+
   await app.listen(3000);
 }
 bootstrap();
