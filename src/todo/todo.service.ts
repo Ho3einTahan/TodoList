a66bdec88bca7877883c6d7ed3d1f6e47c132e9c
ho3eintahan <hoseintahan84@gmail.com>
2025-05-16 12:18:27 +0330

    Update     todo.service.ts

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Todo } from './schema/todo.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+
+    constructor(@InjectModel(Todo.name) private todoModel: Model<Todo>) { }
+
+    async createTodo(todo: Todo): Promise<Todo> {
+        const newTodo = new this.todoModel(todo);
+        return newTodo.save();
+    }
+
+    async getAllTodos(): Promise<Todo[]> {
+        return this.todoModel.find().exec();
+    }
+
+    async getTodoById(id: string): Promise<Todo> {
+        return this.todoModel.findById(id).exec();
+    }
+
+    async updateTodo(id: string, todo: Partial<Todo>): Promise<Todo> {
+        return this.todoModel.findByIdAndUpdate(id, todo, { new: true }).exec();
+    }
+
+    async deleteTodo(id: string): Promise<Todo> {
+        return this.todoModel.findByIdAndDelete(id).exec();
+    }
+
+}
